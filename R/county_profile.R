a3686cf6f7cc3c3d5abfc3bf7dd4aa5284870eb9
robkemp <rjameskemp@gmail.com>
2016-03-07 12:57:04 -0700

    tidyed up code in functions and finished catching up vignette

--- R/county_profile.R
+++ R/county_profile.R
@@ -17,54 +17,48 @@
 #' @export
 
 
-county_profile=function(fips_list, year_list, vars=""){
-  # subject to change, but this is the server URL
-  url_stub="http://red-meteor-147235.nitrousapp.com:4001/profile?"
-
-  # Type checks for each list so that we don't get any text sent to the API for a SQL Injection.
-  if (!is.numeric(fips_list))
-    stop("FIPS should be numeric.")
-  if (!is.numeric(year_list))
-    stop("Years should be numeric.")
-
-  if(any(year_list<1985) || any(year_list>2014))
-    stop("One or more year is out of range. Years should be between 1985 and 2014")
-
-  # Checks for two special call types 300 is for all counties, 0 is for state total, puts proper list together for call
-  suppressWarnings(if (fips_list==300 | fips_list ==0) {
-    fips=c(1,3,5,7,9,11,13,14,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,
-           51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,
-           99,101,103,105,107,109,111,113,115,117,119,121,123,125)
-  } else {
-
-    fips=fips_list
-  }
-)
-  # Creates the URL for the API call
-  call=paste0(url_stub,
-              "&county=", paste(fips, collapse=","),
-              "&year=", paste(year_list, collapse=","),
-              "&vars=", paste(vars, collapse = ","))
-
-  # Makes the API call and converts the JSON to a data frame
-  data=county_names%>%
-    inner_join(jsonlite::fromJSON(call, simplifyVector = TRUE), by="countyfips")
-
-  # Checks if the State Total call (0) is used and if so creates the total, otherwise fixes the type and naming for totalPopulation
-  suppressWarnings(if(fips_list==0){
-    data=data%>%
-      select(-vacancyrate, -householdsize)%>%
-      tidyr::gather(variable, value, -countyfips:-year)%>%
-      group_by(year, variable)%>%
-      summarize(value=sum(as.numeric(value)))%>%
-      tidyr::spread(variable, value)%>%
-      mutate(householdsize=round(householdpopulation/households, 2),
-             vacancyrate=round((vacanthousingunits/totalhousingunits)*100,2))
-    data=bind_cols(data.frame(countyfips=0), data)
-  } else {
-    data=data
-  }
-)
-  # tells the function what to return and changes it from a dplyr tbl object back to a generic data frame
-  return(as.data.frame(data))
-}
+county_profile = function(fips_list, year_list, vars = "") {
+    # subject to change, but this is the server URL
+    url_stub = "http://red-meteor-147235.nitrousapp.com:4001/profile?"
+    
+    # Type checks for each list so that we don't get any text sent to the API for a SQL Injection.
+    if (!is.numeric(fips_list)) 
+        stop("FIPS should be numeric.")
+    if (!is.numeric(year_list)) 
+        stop("Years should be numeric.")
+    
+    if (any(year_list < 1985) || any(year_list > 2014)) 
+        stop("One or more year is out of range. Years should be between 1985 and 2014")
+    
+    # Checks for two special call types 300 is for all counties, 0 is for state total, puts proper list
+    # together for call
+    suppressWarnings(if (fips_list == 300 | fips_list == 0) {
+        fips = c(1, 3, 5, 7, 9, 11, 13, 14, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 
+            45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 
+            91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125)
+    } else {
+        
+        fips = fips_list
+    })
+    # Creates the URL for the API call
+    call = paste0(url_stub, "&county=", paste(fips, collapse = ","), "&year=", paste(year_list, collapse = ","), 
+        "&vars=", paste(vars, collapse = ","))
+    
+    # Makes the API call and converts the JSON to a data frame
+    data = county_names %>% inner_join(jsonlite::fromJSON(call, simplifyVector = TRUE), by = "countyfips")
+    
+    # Checks if the State Total call (0) is used and if so creates the total, otherwise fixes the type and
+    # naming for totalPopulation
+    suppressWarnings(if (fips_list == 0) {
+        data = data %>% select(-vacancyrate, -householdsize) %>% tidyr::gather(variable, value, -countyfips:-year) %>% 
+            group_by(year, variable) %>% summarize(value = sum(as.numeric(value))) %>% tidyr::spread(variable, 
+            value) %>% mutate(householdsize = round(householdpopulation/households, 2), vacancyrate = round((vacanthousingunits/totalhousingunits) * 
+            100, 2))
+        data = bind_cols(data.frame(countyfips = 0), data)
+    } else {
+        data = data
+    })
+    # tells the function what to return and changes it from a dplyr tbl object back to a generic data
+    # frame
+    return(as.data.frame(data))
+} 
--- R/muni_est.r
+++ R/muni_est.r
@@ -8,48 +8,45 @@
 #' @param fips_list Numeric FIPS code(s) for the municipality(ies). Leave blank and specify county to get all places in a county Defaults to blank
 #' @param year_list Numeric list of years between 2010 and 2014
 #' @param county county FIPS number used to pull either parts of a muni or all munis in a county if fips_list is blank Defaults to Blank
-#' @param vars list of variables to pull includes: totalpopulation, householdpopulation, groupquarterspopulation, totalhousingunits, occupiedhousingunits, and vacanthousingunits Defaults to totalpopulation
-#' @param totals controls whether API returns totals or parts of municipalities Defaults to "yes" to give totals not parts.
+#' @param vars list of variables to pull includes: totalpopulation, householdpopulation, groupquarterspopulation, householdsize totalhousingunits, occupiedhousingunits, and vacanthousingunits Defaults to totalpopulation
+#' @param totals controls whether API returns totals or parts of municipalities Defaults to 'yes' to give totals not parts.
 #' @importFrom jsonlite fromJSON
 #' @import dplyr
 #'
 #' @export
 
 
-muni_est=function(fips_list="", year_list, county="", vars="totalpopulation", totals="yes" ){
-  # subject to change, but this is the server URL
-  url_stub="http://red-meteor-147235.nitrousapp.com:4000/munipophousing?"
-
-  # Type checks for each list so that we don't get any text sent to the API for a SQL Injection.
-  if (!is.numeric(fips_list) && fips_list!="")
-    stop("FIPS should be numeric or blank.")
-
-  if (!is.numeric(year_list))
-    stop("Years should be numeric.")
-
-  if(county!="")
-    totals="no"
-
-  if(any(year_list<2010) || any(year_list>2014))
-    stop("One or more year is out of range. Years should be between 2010 and 2014")
-
-  # Creates the URL for the API call
-  call=paste0(url_stub,"year=", paste(year_list, collapse=","),
-              "&placefips=", paste(fips_list, collapse=","),
-              "&countyfips=", county,
-              "&stats=", paste(vars, collapse=","),
-              "&compressed=",totals)
-
-  # Makes the API call and converts the JSON to a data frame
-  data=jsonlite::fromJSON(call, simplifyVector = TRUE)
-  if(totals=="yes"){
-  data=mutate(data, totalpopulation=as.numeric(totalpopulation))%>%
-      rename(municipality=municipalityname)
-  } else {
-    data=mutate(data, totalpopulation=as.numeric(totalpopulation))%>%
-      rename(municipality=municipalityname)
-  }
-  # tells the function what to return and changes it from a dplyr tbl object back to a generic data frame
-  return(as.data.frame(data))
-
-}
+muni_est = function(fips_list = "", year_list, county = "", vars = "totalpopulation", totals = "yes") {
+    # subject to change, but this is the server URL
+    url_stub = "http://red-meteor-147235.nitrousapp.com:4000/munipophousing?"
+    
+    # Type checks for each list so that we don't get any text sent to the API for a SQL Injection.
+    if (!is.numeric(fips_list) && fips_list != "") 
+        stop("FIPS should be numeric or blank.")
+    
+    if (!is.numeric(year_list)) 
+        stop("Years should be numeric.")
+    
+    if (county != "") 
+        totals = "no"
+    
+    if (any(year_list < 2010) || any(year_list > 2014)) 
+        stop("One or more year is out of range. Years should be between 2010 and 2014")
+    
+    # Creates the URL for the API call
+    call = paste0(url_stub, "year=", paste(year_list, collapse = ","), "&placefips=", paste(fips_list, 
+        collapse = ","), "&countyfips=", county, "&stats=", paste(vars, collapse = ","), "&compressed=", 
+        totals)
+    
+    # Makes the API call and converts the JSON to a data frame
+    data = jsonlite::fromJSON(call, simplifyVector = TRUE)
+    if (totals == "yes") {
+        data = mutate(data, totalpopulation = as.numeric(totalpopulation)) %>% rename(municipality = municipalityname)
+    } else {
+        data = mutate(data, totalpopulation = as.numeric(totalpopulation)) %>% rename(municipality = municipalityname)
+    }
+    # tells the function what to return and changes it from a dplyr tbl object back to a generic data
+    # frame
+    return(as.data.frame(data))
+    
+} 
