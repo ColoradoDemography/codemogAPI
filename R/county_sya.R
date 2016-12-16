6f1712e8b051eb0ba69eb191d49265aaad4346a2
robkemp <rjameskemp@gmail.com>
2016-12-16 11:21:33 -0700

    fixed url call for state

--- R/county_sya.R
+++ R/county_sya.R
@@ -36,9 +36,7 @@ if (any(year_list < 1985) || any(year_list > 3000))
 # Checks for two special call types 300 is for all counties, 0 is for state total, puts proper list
 # together for call
 suppressWarnings(if (fips_list == 300 | fips_list == 0) {
-  fips = c(1, 3, 5, 7, 9, 11, 13, 14, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43,
-           45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89,
-           91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125)
+  fips=c(1,3,5,7,9,11,13,14,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125)
 } else {
 
   fips = fips_list
@@ -59,13 +57,13 @@ suppressWarnings(if (year_list == 3000) {
 
 
 suppressWarnings(if (fips_list == 0) {
-  call = paste0(url_stub, "&county=", paste(fips, collapse = ","), "&year=", paste(year, collapse = ","), "&choice='single'", "&group=opt3")
+  call = paste0(url_stub, "county=", paste(fips,collapse = ","), "&year=",paste(year, collapse = ","), "&choice=single", "&group=opt3")
   data = jsonlite::fromJSON(call, simplifyVector = TRUE)
   data$countyfips=rep(0, length(data$year))
   data$county=rep("Colorado", length(data$year))
 
 }else{
-  call = paste0(url_stub, "&county=", paste(fips, collapse = ","), "&year=", paste(year, collapse = ","),"&choice=single")
+  call = paste0(url_stub, "county=", paste(fips, collapse = ","), "&year=", paste(year, collapse = ","),"&choice=single")
   # Makes the API call and converts the JSON to a data frame
   data = jsonlite::fromJSON(call, simplifyVector = TRUE)
 })
